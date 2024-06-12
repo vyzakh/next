29c9a12dfbbdfcfad8ade7b82372d7de8d801433
vyzakh <vysakh.mp@zoft.care>
2024-06-12 11:11:47 +0000

    title change

---
+++ .scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started..."
+
+# Pull the latest version of the app
+git pull origin main
+echo "New changes copied to server !"
+
+echo "Installing Dependencies..."
+npm install --yes
+
+echo "Creating Production Build..."
+# For ReactJS VueJS and Nuxt JS
+npm run build
+
+# For NextJS
+echo "creating Nextjs"
+pm2 start 'npm run start' --name 'vysakh'
+# npm run export
+
+echo "Deployment Finished!"
